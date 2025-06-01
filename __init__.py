793df3f450b96fb7a022fe7c7ad93760bf9b805a
bikiam <157801122+bikiam@users.noreply.github.com>
2025-06-01 16:37:56 +0530

    Update __init__.py
    1 add  code

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
+from .Whisper_text_srt import WhisperAudioToSRTText
+# Node registration
+NODE_CLASS_MAPPINGS = {
+    "WhisperAudioToSRTText": WhisperAudioToSRTText
+}
 
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "WhisperAudioToSRTText": "🗣️Whisper Audio to Text+SRT"
+}
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
